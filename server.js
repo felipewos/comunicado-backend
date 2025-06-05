f47b4921905ee44ebefe3fd32bade2bebecaaa86
felipewagner <130522595+felipewos@users.noreply.github.com>
2025-06-05 17:54:45 -0300

    Update server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 
 app.post('/enviar-comunicado', async (req, res) => {
-  const { nomeArquivo, pdfBase64, destinatario, nome, numero } = req.body;
+  const { nomeArquivo, pdfBase64, destinatario, nome, numero, tipo } = req.body;
 
   const transporter = nodemailer.createTransport({
     service: 'Outlook365',
@@ -19,11 +19,14 @@ app.post('/enviar-comunicado', async (req, res) => {
     }
   });
 
-  const mailOptions = {
-    from: `"Setor de Disciplina" <${process.env.EMAIL_USER}>`,
-    to: destinatario,
-    subject: `Solicitação de Assinatura de Advertência`,
-    text: `Prezado(a) Coordenador(a),
+  let mailOptions;
+
+  if (tipo === "Advertência") {
+    mailOptions = {
+      from: `"Setor de Disciplina" <${process.env.EMAIL_USER}>`,
+      to: destinatario,
+      subject: `Solicitação de Assinatura de Advertência`,
+      text: `Prezado(a) Coordenador(a),
 
 Encaminho, em anexo, o Comunicado de Advertência nº ${numero}, referente ao(à) aluno(a) ${nome}, para sua ciência e assinatura.
 
@@ -40,14 +43,45 @@ Chefe do Setor de Disciplina
 Campus Petrópolis
 (24)2292-9718
 www.cefet-rj.br`,
-    attachments: [
-      {
-        filename: nomeArquivo,
-        content: Buffer.from(pdfBase64, 'base64'),
-        encoding: 'base64'
-      }
-    ]
-  };
+      attachments: [
+        {
+          filename: nomeArquivo,
+          content: Buffer.from(pdfBase64, 'base64'),
+          encoding: 'base64'
+        }
+      ]
+    };
+  } else if (tipo === "Suspensão") {
+    mailOptions = {
+      from: `"Setor de Disciplina" <${process.env.EMAIL_USER}>`,
+      to: destinatario,
+      subject: `Solicitação de Assinaturas de Suspensão`,
+      text: `Prezados(as) Gerente Administrativo(a) e Coordenador(a),
+
+Encaminho, em anexo, o Comunicado de Suspensão nº ${numero}, referente ao(à) aluno(a) ${nome}, para suas ciências e assinaturas.
+
+Solicito que, por gentileza, revise e assine o documento, a fim de prosseguirmos com os trâmites internos.
+
+Este e-mail também foi encaminhado à SAPED para ciência.
+
+Atenciosamente,
+
+Felipe Wagner de O. e Silva
+Chefe do Setor de Disciplina
+Campus Petrópolis
+(24)2292-9718
+www.cefet-rj.br`,
+      attachments: [
+        {
+          filename: nomeArquivo,
+          content: Buffer.from(pdfBase64, 'base64'),
+          encoding: 'base64'
+        }
+      ]
+    };
+  } else {
+    return res.status(400).json({ erro: 'Tipo de comunicado inválido.' });
+  }
 
   try {
     await transporter.sendMail(mailOptions);
